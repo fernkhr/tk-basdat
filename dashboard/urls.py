cd71d0e7f3427c832d5919ba67a886fd86ce1fe8
fernkhr <fern.khairunnisha@ui.ac.id>
2024-11-16 22:46:11 +0700

    update login & dashboard profile

---
+++ dashboard/urls.py
@@ -0,0 +1,10 @@
+# urls.py
+from django.urls import path
+from . import views
+
+app_name = "dashboard"
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    # Rute lainnya...
+]
